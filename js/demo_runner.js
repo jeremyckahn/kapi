acd920cfc3489c628147fc0f0b996c14571b3ffb
JKahn <jkahn@JKahn-MacBook-Pro.local>
2011-02-09 08:53:32 -0600

    Did some cleanup on the demo code.

--- js/demo_runner.js
+++ js/demo_runner.js
@@ -11,32 +11,31 @@ $(function () {
 			fRate: 30,
 			// Set up some basic styles for the canvas
 			styles: {
-				height: '400px',
-				width: '500px',
+				height: 	'400px',
+				width: 		'500px',
 				background: '#333'
 			}
 		}).play(); // Start the animation
 		
 		// The actor `circle` is defined in shapes.js
 		circle1 = myKapi.add(circle, {
-			name: 'circle1',
-			x: 70,
-			y: 70,
+			name: 		'circle1',
+			x: 		70,
+			y: 		70,
 			radius: 50,
-			color: '#00ffaa',
-			easing : 'easeInOutQuint'
+			color: 	'#00ffaa',
+			easing: 'easeInOutQuint'
 		}).keyframe(0, {
 			// Blank, defining keyframe zero as the same as the initial state
 		}).keyframe('3s', {
-			x: 300,
-			y: 300
-		}).liveCopy('3.25s', 
-			'3s'
-		).keyframe('6s', {
-			x: '+=50',
-			y: 100
-		}).liveCopy('8s', 
-			0);
+			x: 	300,
+			y: 	300
+		}).keyframe('3.25s', {
+			// Empty keyframes act as a "wait"
+		}).keyframe('6s', {
+			x: 	'+=50',
+			y: 	100
+		}).liveCopy('8s', 0);
 		
 		// Some jQuery code to attach a click event for pausing.
 		// It is not necessary to get the above code to run.
