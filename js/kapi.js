3fd2cd92de270f9a4329f7a99df7207eaa9193be
Jeremy Kahn <jeremyckahn@gmail.com>
2011-03-26 14:29:16 -0500

    Added documentation for actor.removeAll() and added a test for actor.endCurrentAction().  Moved to v0.6.0.

--- js/kapi.js
+++ js/kapi.js
@@ -2,7 +2,7 @@
 
 /**
  * Kapi - A keyframe API
- * v0.5.0
+ * v0.6.0
  * by Jeremy Kahn - jeremyckahn@gmail.com
  * hosted at: https://github.com/jeremyckahn/kapi
  * 
@@ -48,7 +48,7 @@
  */
 function kapi(canvas, params, events) {
 
-	var version = '0.5.0',
+	var version = '0.6.0',
 		defaults = {
 			'fRate': 20,
 			'autoclear': true
@@ -1676,6 +1676,10 @@ function kapi(canvas, params, events) {
 				return this;
 			};
 
+			/**
+			 * Removes the actor from all keyframes.
+			 * @returns {Object} The actor Object (for chaining).
+			 */
 			actorObj.removeAll = function removeAll () {
 				var id = actorObj.id;
 				
