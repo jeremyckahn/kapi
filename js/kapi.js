ce3783570511af21d4b410b060701119462db2bf
Jeremy Kahn <jeremyckahn@gmail.com>
2011-05-03 22:28:20 -0500

    Incrementing to 1.0.6.

--- js/kapi.js
+++ js/kapi.js
@@ -2,7 +2,7 @@
 
 /**
  * Kapi - A keyframe API
- * v1.0.5
+ * v1.0.6
  * by Jeremy Kahn - jeremyckahn@gmail.com
  * Maintained at: https://github.com/jeremyckahn/kapi
  * 
@@ -50,7 +50,7 @@
  */
 function kapi(canvas, params, events) {
 
-	var version = '1.0.5',
+	var version = '1.0.6',
 		defaults = {
 			'fps': 20,
 			'autoclear': true,
