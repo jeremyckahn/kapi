27e104b970e3859e124157494ec79faa87ee1849
Jeremy Kahn <jeremyckahn@gmail.com>
2011-05-15 17:21:32 -0500

    Made some skeleton code for the Kapi Sapi.

--- ext/sequence/kapi.sequence.js
+++ ext/sequence/kapi.sequence.js
@@ -4,7 +4,53 @@
 	var _private;
 	
 	kapi.fn.sequence = {
-		_create: function create () {
+		/**
+		 * @param {String} sequenceName
+		 * @param {Function} sequence
+		 */
+		_create: function create (sequenceName, sequence) {
+			var actorSequenceName,
+				actorTemplate;
+			
+			actorSequenceName = '_sapi.' + sequenceName;
+			
+			actorTemplate = {
+				setup: function () {
+					
+				},
+				
+				draw: function () {
+					
+				},
+				
+				teardown: function () {
+					
+				}
+			};
+			
+			
+		},
+		
+		/**
+		 * @param {String} sequenceName
+		 * @param {String|Number} keyframeId
+		 */
+		_startAt: function startAt (sequenceName, keyframeId) {
+			
+		},
+		
+		/**
+		 * @param {String} sequenceName
+		 * @param {String|Number} keyframeId
+		 */
+		_endAt: function endAt (sequenceName, keyframeId) {
+			
+		},
+		
+		/**
+		 * @param {String} sequenceName
+		 */
+		_destroy: function destroy (sequenceName) {
 			
 		}
 	};
